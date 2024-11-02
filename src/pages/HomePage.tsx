3b5a26355e5da40cb94839001a85735aff5df012
AnthonySaldana <anthonywebsol@gmail.com>
2024-11-02 15:03:10 -0700

    Adjust home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
-import landingImage from "../assets/landing.png";
-import appDownloadImage from "../assets/appDownload.png";
+// import landingImage from "../assets/landing.png";
+// import appDownloadImage from "../assets/appDownload.png";
 import SearchBar, { SearchForm } from "@/components/SearchBar";
 import { useNavigate } from "react-router-dom";
 
@@ -16,15 +16,15 @@ const HomePage = () => {
     <div className="flex flex-col gap-12">
       <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
         <h1 className="text-5xl font-bold tracking-tight text-orange-600">
-          Tuck into a takeway today
+          Search Fitbite Meal Plans
         </h1>
-        <span className="text-xl">Food is just a click away!</span>
+        <span className="text-xl">A better life is just a click away!</span>
         <SearchBar
           placeHolder="Search by City or Town"
           onSubmit={handleSearchSubmit}
         />
       </div>
-      <div className="grid md:grid-cols-2 gap-5">
+      {/* <div className="grid md:grid-cols-2 gap-5">
         <img src={landingImage} />
         <div className="flex flex-col items-center justify-center gap-4 text-center">
           <span className="font-bold text-3xl tracking-tighter">
@@ -36,7 +36,7 @@ const HomePage = () => {
           </span>
           <img src={appDownloadImage} />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
