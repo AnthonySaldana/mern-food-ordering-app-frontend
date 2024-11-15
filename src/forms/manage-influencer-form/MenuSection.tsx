38552f477bfec68133c074245aeea5d035cc1771
Anthony saldana <anthonywebsol@gmail.com>
2024-11-15 13:34:36 -0800

    set default meal plan index

--- src/forms/manage-influencer-form/MenuSection.tsx
+++ src/forms/manage-influencer-form/MenuSection.tsx
@@ -26,6 +26,7 @@ const MenuSection = () => {
           <FormItem className="flex flex-col gap-2">
             {fields.map((_, index) => (
               <MenuItemInput
+                mealPlanIndex={0}
                 index={index}
                 removeMenuItem={() => remove(index)}
               />
