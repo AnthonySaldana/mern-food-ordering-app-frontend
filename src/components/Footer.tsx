8ebd0e6d4f55fbf3e50e54eb107679f706dd109e
Anthony saldana <anthonywebsol@gmail.com>
2025-01-16 21:55:25 -0800

    fix missing logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="bg-[#09C274] py-10">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <div className="flex items-center gap-2">
-          <img src="/logo.png" alt="FitBite Logo" className="h-8 w-8" />
+          {/* <img src="/logo.png" alt="FitBite Logo" className="h-8 w-8" /> */}
           <span className="text-3xl text-white font-bold tracking-tight">
             FitBite
           </span>
